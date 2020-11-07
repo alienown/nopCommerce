58905f5c27c29ce14c5b0dd8458933ed265f74e9
Kamil Kapliński <alienown@o2.pl>
2020-11-07 14:07:18 +0100

    Plugin init

---
+++ src/Plugins/Nop.Plugin.Misc.TaskManagement/TaskManagementPlugin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Nop.Services.Common;
+using Nop.Services.Plugins;
+
+namespace Nop.Plugin.Misc.TaskManagement
+{
+    public class TaskManagementPlugin : BasePlugin, IMiscPlugin
+    {
+        private readonly string _configurationPageUrl = "Admin/Task/Configure";
+
+        public TaskManagementPlugin()
+        {
+
+        }
+
+        public override string GetConfigurationPageUrl()
+        {
+            return _configurationPageUrl;
+        }
+    }
+}
